1c7142ab599407adcafe670e5d800088c982ff57
hamza <12.hamza.sherif@gmail.com>
2025-06-01 10:34:36 +0300

    Add files via upload

---
+++ Deci-Learnings/script.js
@@ -0,0 +1,9 @@
+
+const websitesDesigned = document.querySelector(".websites-designed");
+const appsdeveloped = document.querySelector(".apps-developed");
+
+setTimeout(() => {
+  websitesDesigned.innerHTML = "4";
+  appsdeveloped.innerHTML = "10";
+}, 400);
+
